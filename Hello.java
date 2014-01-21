d013662334b3b3add909c6e42ca8b0b332f893a0
Howard <H.Cookie09@yahoo.com>
2014-01-21 16:12:39 +0000

    Hello Test Upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+import lejos.nxt.Button;
+import lejos.util.Delay;
+
+public class Hello {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello World");
+		Delay.msDelay(500);
+		
+	}
+
+}
