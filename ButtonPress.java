4c3f98fcb4ec27a4c23cbc2b25b589d1f31202c9
Howard <H.Cookie09@yahoo.com>
2014-01-21 16:20:43 +0000

    Button Press Class

---
+++ ButtonPress.java
@@ -0,0 +1,12 @@
+import lejos.nxt.Button;
+
+public class ButtonPress {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Hello World");
+		Button.waitForAnyPress();
+		
+	}
+
+}
